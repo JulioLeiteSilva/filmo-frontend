1ffe80632d8b4af6134c201c6a0334a4972bdd93
fermiwa <104083221+fermiwa@users.noreply.github.com>
2024-03-28 15:45:46 -0300

    Desenvolvimento da tela de login

--- Filmo/lib/main.dart
+++ Filmo/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:filmo/filmo_app.dart';
 import 'package:flutter/material.dart';
+import 'package:filmo/view/screens/signin.dart';
+
 
 void main() {
   runApp(const FilmoApp());
 }
-
-meu fone acabou a bateria, nõa to ouvindo nada 
--- Filmo/lib/view/screens/signin.dart
+++ Filmo/lib/view/screens/signin.dart
@@ -1,5 +1,3 @@
-import 'dart:js_interop';
-
 import 'package:filmo/mixins/validations_mixin.dart';
 import 'package:filmo/view/components/basic_btn_component.dart';
 import 'package:filmo/view/components/logo_type_component.dart';
@@ -9,48 +7,72 @@ import 'package:flutter/cupertino.dart';
 
 class SignInScreen extends StatelessWidget with ValidationsMixin {
   SignInScreen({super.key});
-  final _emailController = TextEditingController();
-  final formKey = GlobalKey<FormState>;
-  void entrar() {
-    print('Botão "ENTRAR" pressionado');
-  }
-
-  void cadastrar() {
-    print('Botão "ENTRAR" pressionado');
-  }
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+  void entrar() {}
+  void cadastrar() {}
+
+  void teste() {
+      if (_formKey.currentState!.validate()) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Processing Data')),
+        );
+      }
+    }
+
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: LogoTypeComponent(),
-            ),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 120.0),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: LogoTypeComponent(),
+               ),
             Form(
               child: Column(
                 children: [
+                  const SizedBox(height: 50.0),
                   TextInputComponent(
                     controller: _emailController,
                     hintText: "E-mail",
+                    prefixIcon: const Icon(CupertinoIcons.mail),
                     validation: (val) => combine(
-                      [
-                        () => isNotEmpty(val),
-                      ],
+                      [() => isNotEmpty(val),],
                     ),
-                    prefixIcon: Icon(CupertinoIcons.add),
                   ),
+
+                  TextInputComponent(
+                    controller: _passwordController,
+                    hintText: "Senha",
+                    prefixIcon: const Icon(CupertinoIcons.lock_fill),
+                    sufixIcon: IconButton(
+                        icon: const Icon(CupertinoIcons.eye_fill),
+                        onPressed: teste),
+                        isObscureText: true,
+                    validation: isNotEmpty),
+                  
                   BasicBtnComponent(btnText: "ENTRAR", onTap: entrar),
+                  const SizedBox(height: 20.0),
                   BasicBtnComponent(btnText: "CADASTRAR", onTap: cadastrar)
-                ],
-              ),
+                   ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        )
       ),
     );
   }
