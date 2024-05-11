6a0026bb5a6e62267a3a54226e641420ef915d8d
JulinBruto <julioleite93d@gmail.com>
2024-05-11 13:40:13 -0300

    Correções na tela

--- Filmo/lib/view/screens/preferences_screen.dart
+++ Filmo/lib/view/screens/preferences_screen.dart
@@ -3,6 +3,7 @@ import 'package:filmo/view/components/option_btn_component.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 
 class PreferenceScreen extends StatefulWidget {
   const PreferenceScreen({super.key});
@@ -12,52 +13,62 @@ class PreferenceScreen extends StatefulWidget {
 }
 
 class _PreferenceScreenState extends State<PreferenceScreen> {
-  void teste() {}
+  void goToHome() {
+    GoRouter.of(context).push('/');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          child: GridView.count(
-            primary: false,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 2,
-            children: <Widget>[
-              OptionBtnComponent(
-                  onTap: teste,
-                  backgroundImageAsset: 'assets/images/btn_action.png'),
-              OptionBtnComponent(
-                  onTap: teste,
-                  backgroundImageAsset: 'assets/images/btn_animation.png'),
-              OptionBtnComponent(
-                  onTap: teste,
-                  backgroundImageAsset: 'assets/images/btn_comedy.png'),
-              OptionBtnComponent(
-                  onTap: teste,
-                  backgroundImageAsset: 'assets/images/btn_crime.png'),
-              OptionBtnComponent(
-                  onTap: teste,
-                  backgroundImageAsset: 'assets/images/btn_fantasy.png'),
-              OptionBtnComponent(
-                  onTap: teste,
-                  backgroundImageAsset: 'assets/images/btn_ficcion.png'),
-              OptionBtnComponent(
-                  onTap: teste,
-                  backgroundImageAsset: 'assets/images/btn_horror.png'),
-              OptionBtnComponent(
-                  onTap: teste,
-                  backgroundImageAsset: 'assets/images/btn_musical.png'),
-              OptionBtnComponent(
-                  onTap: teste,
-                  backgroundImageAsset: 'assets/images/btn_romance.png'),
-              OptionBtnComponent(
-                  onTap: teste,
-                  backgroundImageAsset: 'assets/images/btn_suspense.png'),
-              BasicBtnComponent(btnText: 'FINALIZAR', onTap: teste)
-            ],
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: SizedBox(
+                child: GridView.count(
+                  primary: false,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    OptionBtnComponent(
+                        onTap: goToHome,
+                        backgroundImageAsset: 'assets/images/btn_action.png'),
+                    OptionBtnComponent(
+                        onTap: goToHome,
+                        backgroundImageAsset:
+                            'assets/images/btn_animation.png'),
+                    OptionBtnComponent(
+                        onTap: goToHome,
+                        backgroundImageAsset: 'assets/images/btn_comedy.png'),
+                    OptionBtnComponent(
+                        onTap: goToHome,
+                        backgroundImageAsset: 'assets/images/btn_crime.png'),
+                    OptionBtnComponent(
+                        onTap: goToHome,
+                        backgroundImageAsset: 'assets/images/btn_fantasy.png'),
+                    OptionBtnComponent(
+                        onTap: goToHome,
+                        backgroundImageAsset: 'assets/images/btn_ficcion.png'),
+                    OptionBtnComponent(
+                        onTap: goToHome,
+                        backgroundImageAsset: 'assets/images/btn_horror.png'),
+                    OptionBtnComponent(
+                        onTap: goToHome,
+                        backgroundImageAsset: 'assets/images/btn_musical.png'),
+                    OptionBtnComponent(
+                        onTap: goToHome,
+                        backgroundImageAsset: 'assets/images/btn_romance.png'),
+                    OptionBtnComponent(
+                        onTap: goToHome,
+                        backgroundImageAsset: 'assets/images/btn_suspense.png'),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
+          BasicBtnComponent(btnText: 'FINALIZAR', onTap: goToHome)
+        ],
       ),
     );
   }
