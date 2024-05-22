73d2eb7294ba92017ec454930f21c185703dc020
JulinBruto <julioleite93d@gmail.com>
2024-05-22 16:25:02 -0300

    Correções

--- Filmo/lib/routes.dart
+++ Filmo/lib/routes.dart
@@ -1,11 +1,18 @@
-
+import 'package:filmo/data/services/auth_service.dart';
 import 'package:filmo/view/screens/main_screen.dart';
 import 'package:filmo/view/screens/sign_in_screen.dart';
 import 'package:filmo/view/screens/sign_up_screen.dart';
 import 'package:go_router/go_router.dart';
 
+final authService = AuthService();
+
 final routes = GoRouter(
   initialLocation: '/',
+  refreshListenable: authService,
+  redirect: ((context, state) {
+    final isAuthenticated = authService.isAuthenticated;
+    final isLoginRoute = state.path == '/login';
+  }),
   routes: [
     GoRoute(
       path: '/',
